bec68d373e360910f98ee5577a38e403b9a4d8d2
Jenef-Great <jenef-great.alemonai@amalitech.com>
2025-09-03 09:09:36 +0000

    accept user input

--- src/lab/Mortgage.java
+++ src/lab/Mortgage.java
@@ -1,4 +1,5 @@
 package lab;
+import java.util.Scanner;
 
 /**
  * principal
@@ -8,10 +9,22 @@ package lab;
  */
 
 public class Mortgage {
-    public static void main(String args[]) {
+    public static void main(String...args) {
+        // adding user inputs
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter amount: ");
+        userInput.nextLine();
 
-        double mortgageAmount = calcMortgage(100000, 4.2, (byte)30);
-        System.out.println("mortgage amount: " + mortgageAmount);
+        System.out.print("Enter rate: ");
+        userInput.nextLine();
+
+        System.out.print("Enter duration period: ");
+        userInput.nextLine();
+
+        userInput.close();
+
+//        double mortgageAmount = calcMortgage(100000, 4.2, (byte)30);
+//        System.out.println("mortgage amount: " + mortgageAmount);
     }
 
     private static double calcMortgage(int principal, double rate, byte time) {
